3ba5ff272b3f094aa0bfdfb863faa870c15c3938
Waseq225 <mohammadwaseq225@gmail.com>
2024-02-10 17:49:39 +1100

    new sql

---
+++ SQL/Problem 607.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT sp.name
+FROM SalesPerson sp
+WHERE sp.name NOT IN
+ (
+     SELECT s.name
+     FROM SalesPerson s
+     JOIN Orders o ON s.sales_id = o.sales_id
+     JOIN Company c ON c.com_id = o.com_id
+     WHERE c.name = 'Red'
+ )
